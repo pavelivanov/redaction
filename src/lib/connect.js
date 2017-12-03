03910dcea75f5160e1575e7a0773365fdc4ca222
Pavel Ivanov <grammka@gmail.com>
2017-12-03 18:07:40 +0300

    update lib connect wrapper to prevent saving resolveStoreProps method to sharable memory variable

--- src/lib/connect.js
+++ src/lib/connect.js
@@ -2,57 +2,53 @@ import { connect } from 'react-redux'
 import { Map } from 'immutable'
 
 
-let _resolveStoreProps
-
-// supports array of strings, strings with dot, or function
-const lookup = (state, ownProps, keyValue) => {
-  if (typeof keyValue === 'function') return keyValue(state, ownProps)
-  if (typeof keyValue === 'string') return _resolveStoreProps(state, keyValue)
-  throw new Error(`Unknown lookup value: ${keyValue}`)
-}
-
-/*
- Takes an object where key is anything you want and value (aka storeProp) is either
- - a dot delimited string
- - array of strings
- - function that returns an array of strings
- It will then dive into an immutable object and grab all of these storeProps
- Returns the same object, but where the values are the resolved data
-*/
-const resolve = (storeProps, state, ownProps) => {
-  const resolved = {}
-
-  for (let key in storeProps) {
-    if (storeProps.hasOwnProperty(key)) {
-      resolved[key] = lookup(state, ownProps, storeProps[key])
-    }
-  }
-
-  return resolved
-}
-
-const mapStateToProps = (storeProps, isConvertFromImmutable) => (state, ownProps) => {
-  let _state = state
-
-  if (isConvertFromImmutable && state instanceof Map) {
-    _state = state.toJS()
-  }
-
-  if (typeof storeProps === 'function') {
-    return storeProps(_state, ownProps)
-  }
-
-  return resolve(storeProps, _state, ownProps)
-}
-
-const defaults = {
-  pure: true,
-  withRef: false,
-}
-
-
 export default (resolveStoreProps, isConvertFromImmutable) => {
-  _resolveStoreProps = resolveStoreProps
+
+  // supports array of strings, strings with dot, or function
+  const lookup = (state, ownProps, keyValue) => {
+    if (typeof keyValue === 'function') return keyValue(state, ownProps)
+    if (typeof keyValue === 'string') return resolveStoreProps(state, keyValue)
+    throw new Error(`Unknown lookup value: ${keyValue}`)
+  }
+
+  /*
+   Takes an object where key is anything you want and value (aka storeProp) is either
+   - a dot delimited string
+   - array of strings
+   - function that returns an array of strings
+   It will then dive into an immutable object and grab all of these storeProps
+   Returns the same object, but where the values are the resolved data
+   */
+  const resolve = (storeProps, state, ownProps) => {
+    const resolved = {}
+
+    for (let key in storeProps) {
+      if (storeProps.hasOwnProperty(key)) {
+        resolved[key] = lookup(state, ownProps, storeProps[key])
+      }
+    }
+
+    return resolved
+  }
+
+  const mapStateToProps = (storeProps, isConvertFromImmutable) => (state, ownProps) => {
+    let _state = state
+
+    if (isConvertFromImmutable && state instanceof Map) {
+      _state = state.toJS()
+    }
+
+    if (typeof storeProps === 'function') {
+      return storeProps(_state, ownProps)
+    }
+
+    return resolve(storeProps, _state, ownProps)
+  }
+
+  const defaults = {
+    pure: true,
+    withRef: false,
+  }
 
   return (storeProps, options) => {
     const isCorrectType = (
